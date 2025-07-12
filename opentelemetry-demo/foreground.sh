7696b8f08c930245c6d9bd50e3d6c18e406c3337
atul sahu <atul.ram@gmail.com>
2025-07-12 11:42:47 +0200

    c

--- opentelemetry-demo/foreground.sh
+++ opentelemetry-demo/foreground.sh
@@ -1,11 +1,16 @@
-echo "Deploying scenario..." && sleep 50
+echo "Deploying scenario..." && sleep 1
 
 
-kubectl wait --for=condition=ready  pod --all -n default
+# This script is used to download the OpenTelemetry demo YAML file.
+wget -O opentelemetry-demo.yaml https://raw.githubusercontent.com/open-telemetry/opentelemetry-demo/refs/heads/main/kubernetes/opentelemetry-demo.yaml
+if [ $? -ne 0 ]; then
+    echo "Failed to download OpenTelemetry demo YAML file."
+    exit 1
+else
+    echo "OpenTelemetry demo YAML file downloaded successfully."
+fi
 
-kubectl get pod -n default
-
-kubectl get svc -n default
-
+# Apply the OpenTelemetry demo YAML file to the Kubernetes cluster.
+kubectl apply -f opentelemetry-demo.yaml
 echo "Scenario deployed successfully."
 

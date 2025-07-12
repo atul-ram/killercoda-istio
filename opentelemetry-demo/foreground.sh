e003753a98d1b20bbef74b0a81308e6016a409d1
atul sahu <atul.ram@gmail.com>
2025-07-12 12:11:26 +0200

    Refactor foreground script to deploy OpenTelemetry demo using Helm and remove YAML download step

--- opentelemetry-demo/foreground.sh
+++ opentelemetry-demo/foreground.sh
@@ -1,19 +1,12 @@
+#!/bin/bash
 echo "Deploying scenario..." && sleep 1
 
+kubectl create ns otel-demo
 
-# This script is used to download the OpenTelemetry demo YAML file.
-wget -O opentelemetry-demo.yaml https://raw.githubusercontent.com/open-telemetry/opentelemetry-demo/refs/heads/main/kubernetes/opentelemetry-demo.yaml
-if [ $? -ne 0 ]; then
-    echo "Failed to download OpenTelemetry demo YAML file."
-    exit 1
-else
-    echo "OpenTelemetry demo YAML file downloaded successfully."
-fi
+# Deploy the scenario using the provided script
+helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
 
-# Apply the OpenTelemetry demo YAML file to the Kubernetes cluster.
-kubectl apply -f opentelemetry-demo.yaml
-
-kubectl wait --for=condition=ready  --timeout=10s pod --all -A
+helm install my-otel-demo open-telemetry/opentelemetry-demo
 
 echo "Scenario deployed successfully."
 

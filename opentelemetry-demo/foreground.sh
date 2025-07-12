433bb5bf5f3798098f804941332f3355ea823b60
atul sahu <atul.ram@gmail.com>
2025-07-12 11:45:01 +0200

    Add wait command for pods to foreground script after applying OpenTelemetry demo YAML

--- opentelemetry-demo/foreground.sh
+++ opentelemetry-demo/foreground.sh
@@ -12,5 +12,8 @@ fi
 
 # Apply the OpenTelemetry demo YAML file to the Kubernetes cluster.
 kubectl apply -f opentelemetry-demo.yaml
+
+kubectl wait --for=condition=ready  --timeout=80s pod --all -A
+
 echo "Scenario deployed successfully."
 
